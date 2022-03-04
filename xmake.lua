709ed7d78536088e18897336b06e7883b47039ef
dev-huangc <huangc@iudge.cn>
2022-03-04 10:42:56 +0800

    fix:工程描述

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,13 @@
-set_config("arch", "x86")
-set_config("mode", "debug")
 if is_os("windows") then
+    set_config("arch", "x86")
     if(is_mode("release")) then
         set_config("vs_runtime", "MD")
         add_cxflags("-MD", {force = true})
+        set_installdir("build/x86/debug")
     else
         set_config("vs_runtime", "MDd")
         add_cxflags("-MDd", {force = true})
+        set_installdir("build/x86/release")
     end
 else
     add_cxflags("-MD", {force = true})
@@ -17,7 +18,10 @@ target("cpp-pm")
     set_kind("binary")
     set_symbols("debug")
     add_packages("vcpkg::boost-asio", "vcpkg::boost-system", "vcpkg::boost-filesystem")
-    add_headerfiles("source/*.h", "source/*.hpp")
+    add_headerfiles("sources/*.h", "sources/*.hpp")
     add_files("sources/*.cpp")
+    after_build(function(target)
+        import("target.action.install")(target)
+    end)
 
     
